f38fbd0d2c20fb0ec3f3e2dc1b0e78fbf390eede
Angron42 <bmaruhnenko@gmail.com>
2022-10-29 16:01:39 +0300

    Update code

--- Лабораторная 9/1.py
+++ Лабораторная 9/1.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-from scipy.interpolate import UnivariateSpline
 import numpy as np
+import scipy.interpolate
 
 x = [0.3,  0.5,  0.8,  1.2,  1.8 ]
 y = [1.19, 2.65, 1.83, 3.84, 2.86]
 
-spl = UnivariateSpline(x, y)
+spl = scipy.interpolate.UnivariateSpline(x, y)
 xs = np.linspace(x[0], x[4], 100)
 
 plt.plot(x, y, 'ro', xs, spl(xs), 'g')
